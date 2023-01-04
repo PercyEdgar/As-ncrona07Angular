de5daa22bb44627ee759e655367bc838c071e2c1
PercyEdgar <1percyqq@gmail.com>
2023-01-03 21:47:09 -0500

    Actividad 07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ElementosModule } from './elementos/elementos.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
-})
+})  
 export class AppModule { }
